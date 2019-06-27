a124ba9a7e62589b04f33bd6b3a4cef316cd766f
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2019-06-27 14:32:36 -0400

    ::arrow_upper_right: styling of home, login, signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -84,7 +84,6 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
     this.http.post('http://localhost:3000/api/user/signup', authData)
       .subscribe((response) => {
-        console.log(response);
         this.router.navigate(['/']);
       });
   }
