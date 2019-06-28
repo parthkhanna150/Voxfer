c4c5203fac6128e677ee0782811658b6ffb67159
blaze077 <shaurya12345678@gmail.com>
2019-06-28 00:04:24 -0400

    Added Forgot Password and all of Signup.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,42 +2,31 @@ const express = require('express');
 const { promisify } = require('util');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
 const User = require('../model/User');
+const randomBytesAsync = promisify(crypto.randomBytes);
 
-//const randomBytesAsync = promisify(crypto.randomBytes);
-router.post('/signup', (req, res, next) => {
-  const sendSignupEmail = (user) => {
-    if (!user) { return; }
-    let transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-      }
+const sendEmail = (mailOptions, success, failure) => {
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+  return transporter.sendMail(mailOptions)
+    .then(() => {
+      console.log('info', success);
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: failure });
     });
-    const mailOptions = {
-      to: 'shaurya12345678@gmail.com',
-      from: process.env.EMAIL_USER,
-      subject: 'Approve User',
-      text: `A new user has joined the website!\n\n
-        Please click on the following link, or paste this into your browser to complete the account activation process:\n\n
-        http://${process.env.FRONTEND_URI}/activate/${user._id}\n\n
-        If you did not request this, please ignore this email and your password will remain unchanged.\n`
-    };
-    return transporter.sendMail(mailOptions)
-      .then(() => {
-        console.log('info', `An e-mail has been sent to ${user.email} with further instructions.`);
-      })
-      .catch((err) => {
-        console.log('ERROR: Could not send activation email.\n', err);
-        console.log('errors', 'Error sending the password reset message. Please try again shortly.');
-        return res.status(500).json({ error: "COuld not send activation email." });
-      });
-  };
+};
 
+router.post('/signup', (req, res, next) => {
   User.findOne({ email: req.body.email }, (err, existingUser) => {
     if (err) { return next(err); }
     if (existingUser) {
@@ -60,7 +49,17 @@ router.post('/signup', (req, res, next) => {
         });
       }
       console.log('Sucessfully registered');
-      sendSignupEmail(newUser).then(() => {
+      const mailOptions = {
+        to: 'shaurya12345678@gmail.com',
+        from: process.env.EMAIL_USER,
+        subject: 'Approve New User',
+        text: `A new user has joined the website!\n\n
+            Please click on the following link, or paste this into your browser to complete the account activation process:\n\n
+            http://${process.env.FRONTEND_URI}/activate/${newUser._id}\n\n`
+      };
+      const success = `The account has been submitted for review.`;
+      const failure = 'Could not send activation email';
+      sendEmail(mailOptions, success, failure).then(() => {
         delete newUser.password;
         res.status(201).json({
           message: 'User created!',
@@ -72,7 +71,22 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.get('/activate/:userId', (req, res, next) => {
-  
+  User.findById(req.params.userId).then(user => {
+    user.active = true;
+    user.save().then(() => {
+      const mailOptions = {
+        to: user.email,
+        from: process.env.EMAIL_USER,
+        subject: 'Account activated!',
+        text: `Your account on SPU has been activated and is read to use.\n\n`
+      };
+      const success = 'User has been notified of account activation.';
+      const failure = 'Failed to send account activation notification email to user.';
+      sendEmail(mailOptions, success, failure).then(() => {
+        return res.status(201).json({ message: 'User has been activated!' });
+      });
+    })
+  })
 });
 
 router.post('/login', (req, res, next) => {
@@ -113,10 +127,57 @@ router.post('/login', (req, res, next) => {
 
 router.post('/reset', (req, res, next) => {
   // this will send an email to this user with a link
-  console.log(req.body.email);
-  res.status(201).json({
-    message: 'Reset!'
+  const createRandomToken = randomBytesAsync(16)
+    .then(buf => buf.toString('hex'));
+
+  const setRandomToken = token =>
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          res.status(500).json({ error: 'Account with that email address does not exist.' });
+          console.log('Email does not exist.');
+        } else {
+          user.passwordResetToken = token;
+          user.passwordResetExpires = Date.now() + 3600000; // 1 hour
+          user = user.save();
+        }
+        return user;
+      });
+  createRandomToken
+    .then(setRandomToken)
+    .then((user) => {
+      const mailOptions = {
+        to: user.email,
+        from: process.env.EMAIL_USER,
+        subject: 'Reset your password on SPU',
+        text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+        Please click on the following link, or paste this into your browser to complete the process:\n\n
+        http://${process.env.FRONTEND_URI}/reset/${user.passwordResetToken}\n\n
+        If you did not request this, please ignore this email and your password will remain unchanged.\n`
+      };
+      const success = `An e-mail has been sent to ${user.email} with further instructions.`;
+      const failure = 'Could not send forgot password email.';
+      sendEmail(mailOptions, success, failure)
+    })
+    .catch(next);
+  return res.status(201).json({
+    message: 'Reset Successful!'
   });
+});
+
+router.get('/reset/:token', (req, res, next) => {
+  // Redirect user back to home page if already logged in
+  User
+    .findOne({ passwordResetToken: req.params.token })
+    .where('passwordResetExpires').gt(Date.now())
+    .exec((err, user) => {
+      if (err) { return res.status(500).json({ error: err }); }
+      if (!user) {
+        console.log('Password reset token is invalid or has expired.');
+        return res.status(500).json({ error: 'Password reset token is invalid or has expired.' });
+      }
+      return res.status(200).json({ message: 'Reset link is valid!' });
+    });
 });
 
 module.exports = router;
